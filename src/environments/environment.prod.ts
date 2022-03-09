be56a541f9e69793155651b86443a2edd8197f17
Herman Andres <hernan.figueroa@novatec.com.co>
2022-03-09 15:09:08 -0500

    Cambio de endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url_server: 'http://asesoresapp-back-miclaro-dev-novatec.104.209.147.150.nip.io/m3/'
+  url_server: 'http://asesoresapp-back-miclaro-dev-novatec.104.209.147.150.nip.io/'
 };
